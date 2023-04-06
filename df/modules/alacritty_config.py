9a79fb62be2ba866b5f7c452a63f48e30615ad7f
Maxim Razantovski <nda.mr43@gmail.com>
2023-04-06 16:05:34 +0200

    alacritty: add config

---
+++ df/modules/alacritty_config.py
@@ -0,0 +1,31 @@
+import df
+from df.config import ModuleConfig
+import platform
+from pathlib import Path
+import io
+
+ID: str = "alacritty_config"
+NAME: str = "Alacritty Config"
+DESCRIPTION: str = "A config for the alacritty terminal emulator"
+DEPENDENCIES: list[str] = ["fira_code_nerd_font"]
+CONFLICTING: list[str] = []
+
+def is_compatible() -> bool | str:
+    return platform.system() in ["Linux", "Darwin"]
+
+def install(config: ModuleConfig, stdout: io.TextIOWrapper) -> None:
+    source_path = df.DOTFILES_PATH / "alacritty"
+    target_path = Path.home() / ".config" / "alacritty"
+
+    df.create_backup(target_path, config, "old_path")
+    df.symlink_path(source_path, target_path)
+
+def uninstall(config: ModuleConfig, stdout: io.TextIOWrapper) -> None:
+    target_path = Path.home() / ".config" / "alacritty"
+    df.restore_backup(target_path, config, "old_path")
+
+def has_update(config: ModuleConfig) -> bool | str:
+    return False
+
+def update(config: ModuleConfig, stdout: io.TextIOWrapper) -> None:
+    pass

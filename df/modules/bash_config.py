e25c7c50b79700056edb617f301da78fb1798461
Maxim Razantovski <nda.mr43@gmail.com>
2023-05-14 14:42:27 +0200

    feat: Add bash config and update starship

---
+++ df/modules/bash_config.py
@@ -0,0 +1,36 @@
+from df.config import ModuleConfig
+import platform
+import tempfile
+import subprocess
+import shutil
+import io
+from pathlib import Path
+import df
+from typing import Union, List
+
+ID: str = "bash_config"
+NAME: str = "Bash Config"
+DESCRIPTION: str = "A simple bash config, incase zsh is not available"
+DEPENDENCIES: List[str] = ["starship_config"]
+CONFLICTING: List[str] = []
+
+target_path = Path.home() / ".bashrc"
+
+def is_compatible() -> Union[bool, str]:
+    return True
+
+def install(config: ModuleConfig, stdout: io.TextIOWrapper) -> None:
+    source_path = df.DOTFILES_PATH / "bash/bashrc"
+    df.create_backup(target_path, config, "old_path")
+    df.symlink_path(source_path, target_path)
+
+def uninstall(config: ModuleConfig, stdout: io.TextIOWrapper) -> None:
+    df.restore_backup(target_path, config, "old_path")
+
+# Optional functions for modules that can be updated
+
+def has_update(config: ModuleConfig) -> Union[bool, str]:
+    return False
+
+def update(config: ModuleConfig, stdout: io.TextIOWrapper) -> None:
+    pass

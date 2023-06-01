36209921a3de6d06ed7c8998e7d5fe856dbab0d5
Maxim Razantovski <nda.mr43@gmail.com>
2023-06-01 13:54:12 +0200

    feat: Add option to add a shortcut for df

---
+++ df/modules/df_shortcut.py
@@ -0,0 +1,43 @@
+from df.config import ModuleConfig
+import df
+import platform
+import tempfile
+import shutil
+import subprocess
+import io
+from pathlib import Path
+from typing import Union, List
+
+ID: str = "df_shortcut"
+NAME: str = "Dotfiles Manager Shortcut"
+DESCRIPTION: str = "Shortcut to launch this dotfiles manager"
+DEPENDENCIES: List[str] = []
+CONFLICTING: List[str] = []
+
+def is_compatible() -> Union[bool, str]:
+    # Only compatible with Linux
+    return platform.system() == "Linux"
+
+def install(config: ModuleConfig, stdout: io.TextIOWrapper) -> None:
+    # Create the desktop file
+    desktop = f"""[Desktop Entry]
+Type=Application
+Name=Dotfiles Manger
+Comment=Install/Uninstall dotfiles
+Exec=python3 {df.DOTFILES_PATH / "dotfiles.py"}
+Terminal=true
+Categories=DesktopSettings;Settings;"""
+    # Write the desktop file (create the directory if it doesn't exist)
+    (Path.home() / ".local" / "share" / "applications").mkdir(parents=True, exist_ok=True)
+    with open(Path.home() / ".local" / "share" / "applications" / "dotfiles.desktop", "w") as f:
+        f.write(desktop)
+
+def uninstall(config: ModuleConfig, stdout: io.TextIOWrapper) -> None:
+    # Just remove the desktop file
+    (Path.home() / ".local" / "share" / "applications" / "dotfiles.desktop").unlink(missing_ok=True)
+
+def has_update(config: ModuleConfig) -> Union[bool, str]:
+    return False
+
+def update(config: ModuleConfig, stdout: io.TextIOWrapper) -> None:
+    pass

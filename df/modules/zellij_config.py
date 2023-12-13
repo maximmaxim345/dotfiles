d2828abfa480a04233c620abc8cf8bb98b4ce4d9
Maxim Razantovski <nda.mr43@gmail.com>
2023-12-13 10:12:15 +0100

    feat: add zellij config

---
+++ df/modules/zellij_config.py
@@ -0,0 +1,37 @@
+import df
+from df.config import ModuleConfig
+import platform
+from pathlib import Path
+import io
+from typing import Union, List
+
+ID: str = "zellij_config"
+NAME: str = "Zellij Config"
+DESCRIPTION: str = "A very basic config for zellij"
+DEPENDENCIES: List[str] = ["zellij"]
+CONFLICTING: List[str] = []
+
+
+def is_compatible() -> Union[bool, str]:
+    return platform.system() in ["Linux", "Darwin"]
+
+
+def install(config: ModuleConfig, stdout: io.TextIOWrapper) -> None:
+    source_path = df.DOTFILES_PATH / "zellij"
+    target_path = Path.home() / ".config" / "zellij"
+
+    df.create_backup(target_path, config, "old_path")
+    df.symlink_path(source_path, target_path)
+
+
+def uninstall(config: ModuleConfig, stdout: io.TextIOWrapper) -> None:
+    target_path = Path.home() / ".config" / "zellij"
+    df.restore_backup(target_path, config, "old_path")
+
+
+def has_update(config: ModuleConfig) -> Union[bool, str]:
+    return False
+
+
+def update(config: ModuleConfig, stdout: io.TextIOWrapper) -> None:
+    pass

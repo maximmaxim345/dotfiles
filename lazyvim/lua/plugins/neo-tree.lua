fe12bee4fde682ebcd2ff88d60a558e2d737a510
Maxim Razantovski <nda.mr43@gmail.com>
2024-03-01 14:24:22 +0100

    feat: remap <leader>e to open root file explorer

--- lazyvim/lua/plugins/neo-tree.lua
+++ lazyvim/lua/plugins/neo-tree.lua
@@ -21,6 +21,35 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     optional = true,
+    keys = function(_, keys)
+      local Util = require("lazyvim.util")
+      local override = {
+        {
+          "<leader>fE",
+          function()
+            require("neo-tree.command").execute({ toggle = true, dir = Util.root() })
+          end,
+          desc = "Explorer NeoTree (root dir)",
+        },
+        {
+          "<leader>fe",
+          function()
+            require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+          end,
+          desc = "Explorer NeoTree (cwd)",
+        },
+        { "<leader>e", "<leader>fe", desc = "Explorer NeoTree (root dir)", remap = true },
+        { "<leader>E", "<leader>fE", desc = "Explorer NeoTree (cwd)", remap = true },
+      }
+      for i, key in ipairs(keys) do
+        for _, over in ipairs(override) do
+          if key[1] == over[1] then
+            keys[i] = over
+            break
+          end
+        end
+      end
+    end,
     opts = function(_, opts)
       opts.window.mappings["-"] = "split_with_window_picker"
       opts.window.mappings["|"] = "vsplit_with_window_picker"

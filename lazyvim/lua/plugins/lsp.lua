fe6494f9613c7d19b63f47a6f331b1e2272e0549
Maxim Raznatovski <nda.mr43@gmail.com>
2024-12-14 12:15:21 +0100

    feat(nvim): port nvim-cmp behaviour to blink.cmp

--- lazyvim/lua/plugins/lsp.lua
+++ lazyvim/lua/plugins/lsp.lua
@@ -5,4 +5,25 @@ return {
       inlay_hints = { enabled = true },
     },
   },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "enter",
+        ["<Tab>"] = {
+          "select_next",
+          "fallback",
+        },
+        ["<S-Tab>"] = {
+          "select_prev",
+          "fallback",
+        },
+      },
+      completion = {
+        list = {
+          selection = "auto_insert",
+        },
+      },
+    },
+  },
 }

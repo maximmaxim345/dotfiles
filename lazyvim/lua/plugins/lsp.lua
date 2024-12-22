7eb535ce6e656391d78a0ec17f766ed058c61ede
Maxim Raznatovski <nda.mr43@gmail.com>
2024-12-22 13:38:26 +0100

    feat(nvim): customize blink.cmp

--- lazyvim/lua/plugins/lsp.lua
+++ lazyvim/lua/plugins/lsp.lua
@@ -21,7 +21,52 @@ return {
       },
       completion = {
         list = {
+          max_items = 200,
           selection = "auto_insert",
+          cycle = {
+            from_bottom = true,
+          },
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+        },
+      },
+      signature = {
+        enabled = true,
+      },
+      fuzzy = {
+        sorts = {
+          function(a, b)
+            local type = require("blink.cmp.types").CompletionItemKind
+
+            local function rerank(kind)
+              -- Reranks completion options according to my preference:
+              -- Text = 1, => Field
+              -- Method = 2, => Method
+              -- Function = 3, => Function
+              -- Constructor = 4, => Text
+              -- Field = 5, => Constructor
+              -- ...
+              if kind == type.Field then
+                return type.Text
+              elseif kind == type.Text then
+                return type.Constructor
+              elseif kind == type.Constructor then
+                return type.Field
+              else
+                return kind
+              end
+            end
+            local a_kind = rerank(a.kind)
+            local b_kind = rerank(b.kind)
+            if a_kind == b_kind then
+              return
+            end
+            return a_kind < b_kind
+          end,
+          "score",
+          "sort_text",
         },
       },
     },

d6fc69a88715de0dededc21589d7d290aa99b044
Maxim Raznatovski <nda.mr43@gmail.com>
2025-01-06 12:43:31 +0100

    fix(nvim): OSC 52 hanging with ssh

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -5,3 +5,22 @@
 -- remove folds saving
 vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" }
 vim.g.snacks_animate = false
+
+-- Clipboard
+if vim.env.SSH_TTY then
+  vim.opt.clipboard:append("unnamedplus")
+  local function paste()
+    return vim.split(vim.fn.getreg(""), "\n")
+  end
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
+end

99fed6d0221ceb9e657342a35b5598f9db44be11
Maxim Raznatovski <nda.mr43@gmail.com>
2025-02-16 10:40:38 +0100

    feat(nvim): add multicursors

--- lazyvim/lua/plugins/buffers.lua
+++ lazyvim/lua/plugins/buffers.lua
@@ -138,5 +138,22 @@ return {
         enabled = false,
       }
     end,
+    {
+      "smoka7/multicursors.nvim",
+      event = "VeryLazy",
+      dependencies = {
+        "nvimtools/hydra.nvim",
+      },
+      opts = {},
+      cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+      keys = {
+        {
+          mode = { "v", "n" },
+          "<Leader>m",
+          "<cmd>MCstart<cr>",
+          desc = "Create a selection for selected text or word under the cursor",
+        },
+      },
+    },
   },
 }

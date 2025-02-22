ff0d2cc6938c292b75db889733e05272df436881
Maxim Raznatovski <nda.mr43@gmail.com>
2025-02-22 13:09:00 +0100

    fix(nvim): remove broken blink reordering

--- lazyvim/lua/plugins/lsp.lua
+++ lazyvim/lua/plugins/lsp.lua
@@ -38,47 +38,6 @@ return {
       signature = {
         enabled = true,
       },
-      fuzzy = {
-        sorts = {
-          function(a, b)
-            local type = require("blink.cmp.types").CompletionItemKind
-
-            local function rerank(kind)
-              -- Reranks completion options according to my preference:
-              -- Text = 1, => Field
-              -- Method = 2, => Variable
-              -- Function = 3, => Method
-              -- Constructor = 4, => Function
-              -- Field = 5, => Text
-              -- Variable = 6 => Constructor
-              -- ...
-              if kind == type.Field then
-                return type.Text
-              elseif kind == type.Variable then
-                return type.Method
-              elseif kind == type.Method then
-                return type.Function
-              elseif kind == type.Function then
-                return type.Constructor
-              elseif kind == type.Text then
-                return type.Field
-              elseif kind == type.Constructor then
-                return type.Variable
-              else
-                return kind
-              end
-            end
-            local a_kind = rerank(a.kind)
-            local b_kind = rerank(b.kind)
-            if a_kind == b_kind then
-              return
-            end
-            return a_kind < b_kind
-          end,
-          "score",
-          "sort_text",
-        },
-      },
     },
   },
 }

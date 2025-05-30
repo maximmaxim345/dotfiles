b64d1f9ced2191a193f2fcfe719f95e3c0ec8efa
Maxim Raznatovski <nda.mr43@gmail.com>
2025-05-30 19:34:09 +0200

    feat(nvim): use `basedpyright` for python

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -2,6 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.g.lazyvim_python_lsp = "basedpyright"
+
 -- remove folds saving
 vim.opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" }
 vim.g.snacks_animate = false
